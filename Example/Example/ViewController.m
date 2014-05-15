2f161992e8ccb18f7659868e74a2fc80e1cdc8ba
Filip Stefansson <filip@pixby.se>
2014-05-15 13:23:23 +0200

    Created the example project.

--- Example/Example/ViewController.m
+++ Example/Example/ViewController.m
@@ -7,9 +7,10 @@
 //
 
 #import "ViewController.h"
+#import "UIButton+tintImage.h"
 
 @interface ViewController ()
-
+@property (weak, nonatomic) IBOutlet UIButton *likeButton;
 @end
 
 @implementation ViewController
@@ -17,7 +18,21 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    
+    // Normal state
+    [self.likeButton setImageTintColor:[UIColor blueColor] forState:UIControlStateNormal];
+    
+    [self.likeButton setTitleColor:[UIColor blueColor] forState:UIControlStateNormal];
+    
+    // Selected state
+    [self.likeButton setImageTintColor:[UIColor redColor] forState:UIControlStateSelected];
+    
+    [self.likeButton setTitleColor:[UIColor redColor] forState:UIControlStateSelected];
+}
+
+- (IBAction)like:(id)sender
+{
+    self.likeButton.selected = !self.likeButton.selected;
 }
 
 - (void)didReceiveMemoryWarning
